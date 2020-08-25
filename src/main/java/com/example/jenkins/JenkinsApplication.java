a32141c1e01e148dd7f9e824467430725c357de1
Vikash <vikash.thakur@vncdigital>
2020-08-25 08:36:31 +0530

    third commit

--- src/main/java/com/example/jenkins/JenkinsApplication.java
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -20,7 +20,7 @@ public class JenkinsApplication {
 	
 	public static void main(String[] args) {
 		logger.info("application executed...");
-		logger.info("application executed..second.");
+
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
 
